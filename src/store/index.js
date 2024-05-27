b9a975cf948bce0e560112eab941d64414051b0c
sidaswal7 <siddhantaswal7@gmail.com>
2024-05-27 12:59:51 +0530

    implemented store

---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+import { configureStore } from "@reduxjs/toolkit"
+
+
+const counterReducer = (state={counter:0}, action)=>{
+    if(action.type === 'increment'){
+        return {
+            counter:state.counter + 1
+        }
+    }
+    if(action.type === 'decrement'){
+        return {
+            counter:state.counter - 1
+        }
+    }
+    if(action.type === 'incrementby2'){
+        return {
+            counter:state.counter + 2
+        }
+    }
+    if(action.type === 'decrementby2'){
+        return {
+            counter:state.counter - 2
+        }
+    }
+    return state
+}
+
+const store = configureStore(counterReducer);
+
+export default store;
